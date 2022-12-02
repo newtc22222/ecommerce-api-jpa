d25e69870f42b929aab3d41007e2d9935d37aa7e
Nhật Phi Võ <64945902+newtc22222@users.noreply.github.com>
2022-12-03 01:40:02 +0700

    create some file for the application

--- web-api/src/main/java/EcommerceApiApplication.java
+++ web-api/src/main/java/EcommerceApiApplication.java
@@ -1,2 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SuppressWarnings("SpringBootApplicationSetup")
+@SpringBootApplication
+@ComponentScan(basePackages = { "com.hcmute.ecommerce" })
 public class EcommerceApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApiApplication.class);
+    }
 }
